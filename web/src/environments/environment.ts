f62cc524b8302038b84e14edec5e73eb8205fa3d
Carlos Ponce <49385124+CarlosPonceDev@users.noreply.github.com>
2020-12-13 12:46:12 -0600

    Remove some stupid commits

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -4,16 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyBcfoJc0yzg9A3BX1PaAngKNKxC1sSSlbI",
-    authDomain: "trasem-fc1209.firebaseapp.com",
-    databaseURL: "https://trasem-fc1209.firebaseio.com",
-    projectId: "trasem-fc1209",
-    storageBucket: "trasem-fc1209.appspot.com",
-    messagingSenderId: "498596713358",
-    appId: "1:498596713358:web:2d630512bbd49b96490adf",
-    measurementId: "G-J0HTD3D2EJ"
-  }
 };
 
 /*
